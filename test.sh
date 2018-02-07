ee93c367f2f4647c71c997f181b600b8980ce244
ephimoff <a.ephimoff@gmail.com>
2018-02-08 10:46:10 +1300

    Fixing Travis builds

--- test.sh
+++ test.sh
@@ -17,7 +17,7 @@ fi
 
 # find and lint files
 for f in $(find . -type f -not \( -iwholename '*.git*' -o -iwholename '*.tmp*'  \) | sort -u) ; do
-    if file "$f" | grep -i --quiet "\(bash\|shell\) script" ; then
+    if file "$f" | grep -i --quiet "\\(bash\\|shell\\) script" ; then
         shellcheck "$f"
     elif file "$f" | grep -i --quiet "text" ; then
         if [[ "$f" = *.md ]]; then
