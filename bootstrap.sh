2d5328b6f7bc2b7cd529d60b19e83a9a9013b9f1
Anton Efimov <anton.efimov@auto1.com>
2019-08-26 15:10:55 +0200

    Updated the paths everywhere

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,12 +1,9 @@
 #!/usr/bin/env bash
 
-echo "Hi! I'm going to install tooling and tweak your system settings."
 echo "During the installation you might need to type the system password several times."
 echo "Note that some of these changes require a logout/restart to take effect."
-echo "Some of the apps require the manual enabling of the accessability settings."
-echo "Here I go..."
 
-directory=~/Projects/dotfiles
+directory=~/Projects/install_dotfiles
 
 $directory/.directories
 $directory/.templates
