0ee52a2fef189368b7b621a40f012ac096f4b8e8
Anton Efimov <anton.efimov@auto1.com>
2019-08-27 14:42:45 +0200

    Fixing running scripts

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,7 +3,7 @@
 echo "During the installation you might need to type the system password several times."
 echo "Note that some of these changes require a logout/restart to take effect."
 
-directory=~/Projects/install_dotfiles
+directory="${HOME}/Projects/install_dotfiles"
 
 dotfiles_repo="https://github.com/ephimoff/dotfiles.git"
 dotfiles_repo_dest="${HOME}/Projects/dotfiles"
@@ -11,14 +11,21 @@ dotfiles_repo_dest="${HOME}/Projects/dotfiles"
 echo "Clonning a dotfiles repository: $dotfiles_repo"
 git clone "$dotfiles_repo" "$dotfiles_repo_dest"
 
-$directory/directories.sh
-$directory/templates.sh
-$directory/macos.sh
-$directory/brew.sh
-# $directory/cask.sh
-$directory/oh_my_zsh.sh
-$directory/dotfiles.sh
-$directory/preferences.sh
+echo "current dir is ______$(pwd)"
+echo "... compare it to ___$directory"
+echo " content of the current directory is: "
+ls -la
+echo "-----------"
+
+
+"$directory/directories.sh"
+"$directory/templates.sh"
+"$directory/macos.sh"
+"$directory/brew.sh"
+# "$directory/cask.sh"
+"$directory/oh_my_zsh.sh"
+"$directory/dotfiles.sh"
+"$directory/preferences.sh"
 
 # Finished
 echo "$(basename "$0") complete."
