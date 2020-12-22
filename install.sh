fbfa076041c75474f20fbcf5bee96326c46e16d1
Anton Efimov <a.ephimoff@gmail.com>
2020-12-22 21:11:47 +0100

    FIxing CI

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# mkdir "Projects"
+mkdir "${HOME}/Projects"
+
+./brew_install.sh
+./clone_dotfiles.sh
+./directories.sh
+./macos.sh
+./brew.sh
+./dotfiles.sh
+./applications.sh
+./oh_my_zsh.sh
+
